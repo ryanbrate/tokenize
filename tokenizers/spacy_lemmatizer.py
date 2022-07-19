e211ba741d37210fff31a325e62a5a40ce9435a9
ryanbrate <r.brate@gmail.com>
2022-07-19 21:37:41 +0200

    ammend tokenizer functions to correctly handle empty documents

--- tokenizers/spacy_lemmatizer.py
+++ tokenizers/spacy_lemmatizer.py
@@ -14,6 +14,14 @@ Return a lemmatized & tokenized version of the tuple of the form ...
         ...
     ]  # sentences
 ]
+
+by default, if list of strings is empty and to retain output consistency, return:
+    [
+        label, 
+        [
+            []
+        ]
+    ]
 """
 
 import spacy
@@ -25,12 +33,18 @@ def tokenize(document: tuple) -> tuple:
     label, list_of_strings = document
 
     sentences = []
-    for s in list_of_strings:
 
-        doc = nlp(s)
-        for sent in doc.sents:
-            tokens: list[str] = [t.lemma_ for t in sent]
-            sentences.append(tokens)
+    if len(list_of_strings) == 0:  # empty list of strings
+        sentences.append([])
+    else:  # non-empty list of strings
+
+        # add sentences, as lists of tokens, to sentences
+        for s in list_of_strings:
+
+            doc = nlp(s)
+            for sent in doc.sents:
+                tokens: list[str] = [t.lemma_ for t in sent]
+                sentences.append(tokens)
 
     # finally:
     return (label, sentences)
--- tokenizers/spacy_tokenizer.py
+++ tokenizers/spacy_tokenizer.py
@@ -32,19 +32,35 @@ def tokenize(document: tuple) -> tuple:
     Return:
         [
             label,
-            # list of sentences as token lists
+            [
+                [],  # a sentence as a list of tokens
+                ...
+            ]  # list of sentences as token lists
+        ]
+
+    by default, if list of strings is empty and to retain output consistency, return:
+        [
+            label, 
+            [
+                []
+            ]
         ]
     """
 
     label, list_of_strings = document
 
     sentences = []
-    for s in list_of_strings:
 
-        doc = nlp(s)
-        for sent in doc.sents:
-            tokens: list[str] = [t.text for t in sent]
-            sentences.append(tokens)
+    if len(list_of_strings) == 0:  # empty list of strings
+        sentences.append([])
+    else:  # non-empty list of strings
+
+        # add sentences, as lists of tokens, to sentences
+        for s in list_of_strings:
+            doc = nlp(s)
+            for sent in doc.sents:
+                tokens: list[str] = [t.text for t in sent]
+                sentences.append(tokens)
 
     # finally:
     return (label, sentences)
